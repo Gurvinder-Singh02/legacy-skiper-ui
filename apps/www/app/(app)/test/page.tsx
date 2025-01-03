d1d6c7421539efa142831a4cccdb9fda25bfe9f9
Gurvinder Singh <gurvindermaksudra@gmail.com>
2025-01-03 23:11:51 +0530

    added size for types

--- apps/www/app/(app)/test/page.tsx
+++ apps/www/app/(app)/test/page.tsx
@@ -5,7 +5,7 @@ import MaskedDiv from "./masked-div"
 function page() {
   return (
     <div className="mt-40 flex h-screen w-screen items-center justify-center">
-      <MaskedDiv variant="type-1" >
+      <MaskedDiv variant="type-3" >
         <div className="size-full bg-red-500" />
       </MaskedDiv>
     </div>
