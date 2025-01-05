ebdcbcc5124b717406ef6ebf3848ef4d94081a94
Gurvinder Singh <gurvindermaksudra@gmail.com>
2025-01-06 00:58:17 +0530

    skiper marqee fix

--- apps/www/registry/default/ui/skiper-marquee.tsx
+++ apps/www/registry/default/ui/skiper-marquee.tsx
@@ -4,11 +4,13 @@ import { useEffect, useId, useRef, useState } from "react"
 import { motion, useAnimation, useInView } from "framer-motion"
 import {
   BarChart,
+  Circle,
   File,
   Globe,
   HeartHandshake,
   Rss,
   Shield,
+  Snowflake,
 } from "lucide-react"
 
 import { cn } from "@/lib/utils"
@@ -65,41 +67,36 @@ function Marquee({
 
 const tiles = [
   {
-    icon: <HeartHandshake className="size-full" />,
+    icon: <Snowflake className="size-full" />,
     bg: (
-      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-orange-600 via-rose-600 to-violet-600 opacity-70 blur-[20px]"></div>
+      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-orange-600 via-rose-600 to-violet-600 opacity-70 "></div>
     ),
   },
   {
-    icon: <Globe className="size-full" />,
+    icon: <Snowflake className="size-full" />,
     bg: (
-      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-cyan-500 via-blue-500 to-indigo-500 opacity-70 blur-[20px]"></div>
+      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-cyan-500 via-blue-500 to-indigo-500 opacity-70 "></div>
     ),
   },
   {
-    icon: <File className="size-full" />,
+    icon: <Snowflake className="size-full" />,
     bg: (
-      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-green-500 via-teal-500 to-emerald-600 opacity-70 blur-[20px]"></div>
+      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-green-500 via-teal-500 to-emerald-600 opacity-70 "></div>
     ),
   },
   {
-    icon: <Shield className="size-full" />,
+    icon: <Snowflake className="size-full" />,
     bg: (
-      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-yellow-400 via-orange-500 to-yellow-600 opacity-70 blur-[20px]"></div>
+      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-yellow-400 via-orange-500 to-yellow-600 opacity-70 "></div>
     ),
   },
   {
-    icon: <Rss className="size-full" />,
+    icon: <Snowflake className="size-full" />,
     bg: (
-      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-orange-600 via-rose-600 to-violet-600 opacity-70 blur-[20px]"></div>
+      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-orange-600 via-rose-600 to-violet-600 opacity-70 "></div>
     ),
   },
-  {
-    icon: <BarChart className="size-full" />,
-    bg: (
-      <div className="pointer-events-none absolute left-1/2 top-1/2 size-1/2 -translate-x-1/2 -translate-y-1/2 overflow-visible rounded-full bg-gradient-to-r from-gray-600 via-gray-500 to-gray-400 opacity-70 blur-[20px]"></div>
-    ),
-  },
+  
 ]
 
 function shuffleArray(array: any[]) {
