2f5a4d9276459bd5d21e5f613d1619cc74706c28
Gurvinder Singh <gurvindermaksudra@gmail.com>
2025-01-12 12:58:20 +0530

    wrap button docs

--- apps/www/components/skiper/wrap-button.tsx
+++ apps/www/components/skiper/wrap-button.tsx
@@ -8,14 +8,15 @@ interface WrapButtonProps {
     className?: string;
     children: React.ReactNode;
     href?: string;
+    bgColor: string;
 }
 
-const WrapButton: React.FC<WrapButtonProps> = ({ className, children, href  }) => {
+const WrapButton: React.FC<WrapButtonProps> = ({ className, children, href, bgColor }) => {
     return (
         <>
             {href ? <Link href={href}>
                 <div className={cn("group cursor-pointer border group border-[#3B3A3A] bg-[#151515] gap-2  h-[64px] flex items-center p-[11px] rounded-full", className)} >
-                    <div className='border border-[#3B3A3A] bg-[#ff3f17]  h-[43px] rounded-full flex items-center justify-center text-white'>
+                    <div className={cn('border border-[#3B3A3A] bg-[#ff3f17]  h-[43px] rounded-full flex items-center justify-center text-white', bgColor)}>
                         <p className='font-medium tracking-tight mr-3 ml-2 flex items-center gap-2 justify-center ' >{children}</p>
                     </div>
                     <div className='text-[#3b3a3a] group-hover:ml-2  ease-in-out transition-all size-[26px] flex items-center justify-center rounded-full border-2 border-[#3b3a3a]  ' >
@@ -23,7 +24,7 @@ const WrapButton: React.FC<WrapButtonProps> = ({ className, children, href  }) =
                     </div>
                 </div >
             </Link > : <div className={cn("group cursor-pointer border group border-[#3B3A3A] bg-[#151515] gap-2  h-[64px] flex items-center p-[11px] rounded-full", className)} >
-                < div className='border border-[#3B3A3A] bg-[#fe7500]  h-[43px] rounded-full flex items-center justify-center text-white' >
+                <div className={cn('border border-[#3B3A3A] bg-[#ff3f17]  h-[43px] rounded-full flex items-center justify-center text-white', bgColor)}>
                     <Globe className='mx-2 animate-spin ' />
                     <p className='font-medium tracking-tight mr-3' >{children}</p>
                 </ div >
