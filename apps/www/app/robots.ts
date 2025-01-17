0fbf8c414afb475f800057a224eab34191671700
Gurvinder Singh <gurvindermaksudra@gmail.com>
2025-01-17 11:37:03 +0530

    robots ts file for seo

---
+++ apps/www/app/robots.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: [
+          '/',          // Homepage
+          '/docs/',     // Documentation
+        ],
+        disallow: [
+          '/private/',  // Private routes
+          '/api/',      // API routes
+        ]
+      }
+    ],
+    sitemap: 'https://skiper-ui.com/sitemap.xml', 
+  }
+}
