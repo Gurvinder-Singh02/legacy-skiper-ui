097a793df9e70b4b3eb2c170b0841f1ff6874932
Gurvinder Singh <gurvindermaksudra@gmail.com>
2025-01-19 13:33:52 +0530

    update ui theme toggle registry directry

--- apps/www/registry/default/ui/theme-animations.ts
+++ apps/www/registry/default/ui/theme-animations.ts
@@ -1,62 +1,56 @@
 // animations.ts
-export type AnimationVariant = "circle" | "circle-blur" | "polygon"
-export type AnimationStart =
-  | "top-left"
-  | "top-right"
-  | "bottom-left"
-  | "bottom-right"
+export type AnimationVariant = "circle" | "circle-blur" | "polygon" | "gif";
+export type AnimationStart = "top-left" | "top-right" | "bottom-left" | "bottom-right" | "center";
 
 interface Animation {
-  name: string
-  css: string
+  name: string;
+  css: string;
 }
 
 const getPositionCoords = (position: AnimationStart) => {
   switch (position) {
-    case "top-left":
-      return { cx: "0", cy: "0" }
-    case "top-right":
-      return { cx: "40", cy: "0" }
-    case "bottom-left":
-      return { cx: "0", cy: "40" }
-    case "bottom-right":
-      return { cx: "40", cy: "40" }
+    case "top-left": return { cx: "0", cy: "0" };
+    case "top-right": return { cx: "40", cy: "0" };
+    case "bottom-left": return { cx: "0", cy: "40" };
+    case "bottom-right": return { cx: "40", cy: "40" };
   }
-}
+};
 
 const generateSVG = (variant: AnimationVariant, start: AnimationStart) => {
-  const { cx, cy } = getPositionCoords(start)
+
+  if (start === "center") return
+
+  const positionCoords = getPositionCoords(start);
+  if (!positionCoords) {
+    throw new Error(`Invalid start position: ${start}`);
+  }
+  const { cx, cy } = positionCoords;
 
   if (variant === "circle") {
-    return `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><circle cx="${cx}" cy="${cy}" r="20" fill="white"/></svg>`
+    return `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><circle cx="${cx}" cy="${cy}" r="20" fill="white"/></svg>`;
   }
 
   if (variant === "circle-blur") {
-    return `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><defs><filter id="blur"><feGaussianBlur stdDeviation="2"/></filter></defs><circle cx="${cx}" cy="${cy}" r="18" fill="white" filter="url(%23blur)"/></svg>`
+    return `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><defs><filter id="blur"><feGaussianBlur stdDeviation="2"/></filter></defs><circle cx="${cx}" cy="${cy}" r="18" fill="white" filter="url(%23blur)"/></svg>`;
   }
 
-  return ""
-}
+  return '';
+};
+
 
 const getTransformOrigin = (start: AnimationStart) => {
   switch (start) {
-    case "top-left":
-      return "top left"
-    case "top-right":
-      return "top right"
-    case "bottom-left":
-      return "bottom left"
-    case "bottom-right":
-      return "bottom right"
+    case "top-left": return "top left";
+    case "top-right": return "top right";
+    case "bottom-left": return "bottom left";
+    case "bottom-right": return "bottom right";
   }
-}
+};
 
-export const createAnimation = (
-  variant: AnimationVariant,
-  start: AnimationStart
-): Animation => {
-  const svg = generateSVG(variant, start)
-  const transformOrigin = getTransformOrigin(start)
+export const createAnimation = (variant: AnimationVariant, start: AnimationStart, url?: string): Animation => {
+
+  const svg = generateSVG(variant, start);
+  const transformOrigin = getTransformOrigin(start);
 
   if (variant === "polygon") {
     return {
@@ -97,9 +91,86 @@ export const createAnimation = (
           clip-path: polygon(171% 50%, 50% 171%, -50% 71%, 50% -71%);
         }
       }
-      `,
-    }
+      `
+    };
   }
+  if (variant === "circle" && start == "center") {
+    return {
+      name: `${variant}-${start}`,
+      css: `
+       ::view-transition-group(root) {
+        animation-duration: 0.7s;
+        animation-timing-function: var(--expo-out);
+      }
+            
+      ::view-transition-new(root) {
+        animation-name: reveal-light;
+      }
+
+      ::view-transition-old(root),
+      .dark::view-transition-old(root) {
+        animation: none;
+        z-index: -1;
+      }
+      .dark::view-transition-new(root) {
+        animation-name: reveal-dark;
+      }
+
+      @keyframes reveal-dark {
+        from {
+          clip-path: circle(0% at 50% 50%);
+        }
+        to {
+          clip-path: circle(100.0% at 50% 50%);
+        }
+      }
+
+      @keyframes reveal-light {
+        from {
+           clip-path: circle(0% at 50% 50%);
+        }
+        to {
+          clip-path: circle(100.0% at 50% 50%);
+        }
+      }
+      `
+    };
+  }
+  if (variant === "gif") {
+    return {
+      name: `${variant}-${start}`,
+      css: `
+      ::view-transition-group(root) {
+  animation-timing-function: var(--expo-in);
+}
+
+::view-transition-new(root) {
+  mask: url('${url}') center / 0 no-repeat;
+  animation: scale 3s;
+}
+
+::view-transition-old(root),
+.dark::view-transition-old(root) {
+  animation: scale 3s;
+}
+
+@keyframes scale {
+  0% {
+    mask-size: 0;
+  }
+  10% {
+    mask-size: 50vmax;
+  }
+  90% {
+    mask-size: 50vmax;
+  }
+  100% {
+    mask-size: 2000vmax;
+  }
+}`
+    };
+  }
+
 
   return {
     name: `${variant}-${start}`,
@@ -108,7 +179,7 @@ export const createAnimation = (
         animation-timing-function: var(--expo-out);
       }
       ::view-transition-new(root) {
-        mask: url('${svg}') ${start.replace("-", " ")} / 0 no-repeat;
+        mask: url('${svg}') ${start.replace('-', ' ')} / 0 no-repeat;
         mask-origin: content-box;
         animation: scale-${start} 1s;
         transform-origin: ${transformOrigin};
@@ -124,6 +195,6 @@ export const createAnimation = (
           mask-size: 350vmax;
         }
       }
-    `,
-  }
-}
+    `
+  };
+};
--- apps/www/registry/default/ui/theme-toggle-button.tsx
+++ apps/www/registry/default/ui/theme-toggle-button.tsx
@@ -15,24 +15,30 @@ import {
 interface ThemeToggleAnimationProps {
   variant?: AnimationVariant
   start?: AnimationStart
+  showLabel?: boolean
+  url ?: string
 }
 
 export default function ThemeToggleButton({
   variant = "circle-blur",
   start = "top-left",
+  showLabel = false,
+  url = ''
 }: ThemeToggleAnimationProps) {
+
   const { theme, setTheme } = useTheme()
-  const [mounted, setMounted] = React.useState(false)
+
   const styleId = "theme-transition-styles"
 
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
 
-  const updateStyles = React.useCallback((css: string) => {
+  const updateStyles = React.useCallback((css: string, name: string) => {
+
     if (typeof window === "undefined") return
 
     let styleElement = document.getElementById(styleId) as HTMLStyleElement
+
+    console.log("style ELement", styleElement)
+    console.log("name", name)
 
     if (!styleElement) {
       styleElement = document.createElement("style")
@@ -41,25 +47,16 @@ export default function ThemeToggleButton({
     }
 
     styleElement.textContent = css
+
+    console.log("content updated")
   }, [])
 
-  React.useEffect(() => {
-    if (!mounted) return
-
-    const animation = createAnimation(variant, start)
-    updateStyles(animation.css)
-
-    return () => {
-      if (typeof window !== "undefined") {
-        const styleElement = document.getElementById(styleId)
-        if (styleElement) {
-          styleElement.remove()
-        }
-      }
-    }
-  }, [variant, start, mounted, updateStyles])
-
   const toggleTheme = React.useCallback(() => {
+
+    const animation = createAnimation(variant, start , url)
+    
+    updateStyles(animation.css, animation.name)
+
     if (typeof window === "undefined") return
 
     const switchTheme = () => {
@@ -76,24 +73,21 @@ export default function ThemeToggleButton({
     document.startViewTransition(switchTheme)
   }, [theme, setTheme])
 
-  if (!mounted) {
-    return (
-      <Button variant="ghost" size="icon" className="w-9 p-0 h-9">
-        <span className="sr-only">Loading theme toggle</span>
-      </Button>
-    )
-  }
 
   return (
     <Button
       onClick={toggleTheme}
       variant="ghost"
       size="icon"
-      className="w-9 p-0 h-9"
+      className="w-9 p-0 h-9 relative group"
     >
       <SunIcon className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
+      {showLabel && <>
+        <span className="hidden group-hover:block border rounded-full px-2 absolute -top-10"> variant = {variant}</span>
+        <span className="hidden group-hover:block border rounded-full px-2 absolute -bottom-10"> start = {start}</span>
+      </>
+      }
     </Button>
   )
 }
