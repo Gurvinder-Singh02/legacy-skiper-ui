c20fafdc9e8f7573ae257909d61c625a91fa67ca
Gurvinder Singh <gurvindermaksudra@gmail.com>
2025-01-20 14:35:36 +0530

    animated number badge fix

--- apps/www/registry/default/example/animated-number-counter-demo.tsx
+++ apps/www/registry/default/example/animated-number-counter-demo.tsx
@@ -12,7 +12,7 @@ export default function AnimatedNumberCounterDemo() {
     <div className="flex flex-col items-center justify-center gap-4">
       <Badge
         variant="outline"
-        className=" rounded-[14px] border border-black/10 text-base text-neutral-800 md:left-6"
+        className=" rounded-[14px] border border-black/10 dark:border-white/20 text-base text-neutral-800 dark:text-white/80 md:left-6"
       >
         <IceCream className="  fill-[#A3C0E0] stroke-1 text-neutral-800" />{" "}
         &nbsp; CountDown Componenet
