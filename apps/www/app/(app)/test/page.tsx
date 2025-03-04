01d2deaa6dd21a3ca3f8db8a2170011747e037c6
Gurvinder-singh02 <gurvindermaksudra@gmail.com>
2025-03-04 21:53:10 +0530

    fix: correct typo in test page description

--- apps/www/app/(app)/test/page.tsx
+++ apps/www/app/(app)/test/page.tsx
@@ -4,7 +4,7 @@ import { Sparkle } from "lucide-react"
 const page = () => {
   return (
     <div className="h-10 w-10 ">
-      <Sparkle /> this is test page
+      <Sparkle /> this is test pageaa
     </div>
   )
 }
